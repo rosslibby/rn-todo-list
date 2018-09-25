8095910a9bc05922113f956b136e2c7de46606ce
Ross Libby <ross@rossdl.com>
2018-09-24 22:39:24 -0500

    started adding create-note view

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, TextInput, View } from 'react-native'
 
 export default class HomeScreen extends Component {
+  constructor () {
+    super ()
+
+    this.state = {
+      text: 'What do you need to do?'
+    }
+  }
+
   render () {
-    return <View><Text>lol</Text></View>
+    const textInputStyle = {
+      backgroundColor: '#fff',
+      paddingTop: 14,
+      paddingBottom: 14,
+      paddingLeft: 30,
+      paddingRight: 30,
+      fontSize: 18,
+      fontWeight: 'thin',
+      color: 'rgba(0, 0, 0, .6)',
+      boxSizing: 'border-box',
+      border: 'none',
+      shadowColor: 'rgba(0, 0, 0, .2)',
+      shadowOffset: {width: 0, height: 0},
+      shadowOpacity: .5,
+      shadowRadius: 2,
+      elevation: 0,
+      marginTop: 20,
+      marginBottom: 20,
+      overflow: 'hidden',
+      borderRadius: 4
+    }
+
+    return <View>
+      <TextInput
+        style={textInputStyle}
+        onChangeText={text => this.setState({text})}
+        value={this.state.text}
+      />
+    </View>
   }
 }
--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { FlatList, Text, View } from 'react-native'
+import { Button, FlatList, Text, View } from 'react-native'
 
 export default class NotesScreen extends Component {
   _completeItem (index) {
@@ -13,10 +13,24 @@ export default class NotesScreen extends Component {
   }
 
   render () {
+    const listStyle = {
+      shadowColor: 'rgba(0, 0, 0, .2)',
+      shadowOffset: {width: 0, height: 0},
+      shadowOpacity: .5,
+      shadowRadius: 2,
+      elevation: 0,
+      marginTop: 20,
+      marginBottom: 20,
+      overflow: 'hidden',
+      borderRadius: 4
+    }
+
     const listItemStyle = {
       display: 'flex',
       backgroundColor: '#ffffff',
-      borderTop: '1pt solid #dddddd',
+      borderColor: '#dddddd',
+      borderStyle: 'solid',
+      borderWidth: .24,
       boxSizing: 'border-box',
       color: '#666',
       listStyle: 'none',
@@ -30,15 +44,19 @@ export default class NotesScreen extends Component {
 
     return <View>
       <FlatList
+        style={listStyle}
+        contentContainerStyle={{ alignItems: 'center' }}
         data={[
           {text: 'This is note A'},
           {text: 'This is note B'},
           {text: 'This is note C'},
           {text: 'This is note D'}
         ]}
-        renderItem={({item, index}) => <Text
-          key={index}
-          style={listItemStyle}>{item.text}</Text>}
+        renderItem={({item, index}) => <View>
+          <Text
+            key={index}
+            style={listItemStyle}>{item.text}</Text>
+        </View>}
       />
     </View>
   }
