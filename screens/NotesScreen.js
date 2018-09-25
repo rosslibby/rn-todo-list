9c7e93d5e60838c6e88fabca5d6bf64c9abcd81a
Ross Libby <ross@rossdl.com>
2018-09-24 22:05:30 -0500

    styled list items

--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -13,6 +13,21 @@ export default class NotesScreen extends Component {
   }
 
   render () {
+    const listItemStyle = {
+      display: 'flex',
+      backgroundColor: '#ffffff',
+      borderTop: '1pt solid #dddddd',
+      boxSizing: 'border-box',
+      color: '#666',
+      listStyle: 'none',
+      paddingTop: 14,
+      paddingBottom: 14,
+      paddingLeft: 30,
+      paddingRight: 30,
+      fontWeight: '100',
+      width: '100%'
+    }
+
     return <View>
       <FlatList
         data={[
@@ -21,7 +36,9 @@ export default class NotesScreen extends Component {
           {text: 'This is note C'},
           {text: 'This is note D'}
         ]}
-        renderItem={({item, index}) => <Text key={index}>{item.text}</Text>}
+        renderItem={({item, index}) => <Text
+          key={index}
+          style={listItemStyle}>{item.text}</Text>}
       />
     </View>
   }
